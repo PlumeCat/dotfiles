730d91d7224001c719b5d9c99d87f114cab25eb9
James <james@james-mbp.local>
2022-05-30 18:46:54 +0100

    init script improvements

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 
-user=$(whoami)
-dotfiles_dir="/home/$user/dotfiles/files"
+dotfiles_dir="$HOME/dotfiles/files"
 
 pushd $dotfiles_dir
 files=$(for f in $(echo .*); do echo $f; done | grep -v -e '^.$' -e '^..$')
 popd
 
 for fname in $files; do
-    ln -s $dotfiles_dir/$fname /home/$user/$fname
+    echo "Linking $fname"
+    ln -s "$dotfiles_dir/$fname" "$HOME/$fname"
 done
 
 
